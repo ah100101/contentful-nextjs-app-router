8a4acf173edd2b7d5eac373bf83de9bc4ac79ba8
Alex <alex.hawley@vercel.com>
2023-09-28 16:51:39 -0500

    updated to post and added header check

--- app/api/revalidation/route.ts
+++ app/api/revalidation/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
-  // const secret = requestHeaders.get('x-vercel-reval-key')
+  const secret = requestHeaders.get("x-vercel-reval-key");
 
-  // if (secret !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
-  //   return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
-  // }
+  if (secret !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
+    return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
+  }
 
   revalidateTag("articles");
 
